2e933f286fc4564f9cdf0a1c8d20d25c651e42cf
Olivia Li <oliviali.ouyang@gmail.com>
2023-03-25 14:51:30 -0700

    Initial Commit

---
+++ nvc-keyboard/nvc_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  nvc_keyboardApp.swift
+//  nvc-keyboard
+//
+//  Created by Olivia Li on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct nvc_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
